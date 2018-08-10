969a1defa307957fbf929efa178ceeea44381072
Luk Burchard <luk.burchard@gmail.com>
2018-08-10 12:02:20 +0200

    Initial Commit

---
+++ background.js
@@ -0,0 +1,78 @@
+const rules = {
+    "m": {
+        redirectUrl: "https://mail.google.com",
+        redirectRegexUrl: "https://mail.google.com/mail/u/0/#search/$1",
+        matchRegex: "(.*)",
+    },
+    "c": {
+        redirectUrl: "https://calendar.google.com",
+        redirectRegexUrl: "https://calendar.google.com/calendar/r/search?q=$1",
+        matchRegex: "(.*)",
+    },
+    "g": {
+        redirectUrl: "https://github.com",
+        redirectRegexUrl: "https://github.com/$1",
+        matchRegex: "(.*)",
+    },
+    "t": {
+        redirectUrl: "https://twitter.com",
+        redirectRegexUrl: "https://twitter.com/$1",
+        matchRegex: "(.*)",
+    },
+    "hn": {
+        redirectUrl: "https://news.ycombinator.com",
+        redirectRegexUrl: "https://hn.algolia.com/?query=$1&sort=byPopularity&prefix&page=0&dateRange=all&type=story",
+        matchRegex: "(.*)",
+    },
+};
+
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+        const url = new URL(details.url);
+
+        // move to static default before function
+        let sr = null;
+        if (syncedRule == null)
+            sr = rules;
+        else
+            sr = syncedRule;
+
+        const target = sr[url.host];
+        if (target != null) {
+            let redirectingURL = target.redirectUrl;
+            if (url.pathname !== "/" && target.matchRegex !== "") {
+                redirectingURL = url.pathname.substr(1).replace(new RegExp(target.matchRegex), target.redirectRegexUrl);
+            }
+            return {
+                redirectUrl: redirectingURL,
+            };
+        }
+        return {};
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking"],
+);
+
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.get('rules', ({rules: loadedRules}) => {
+        if (loadedRules == null) chrome.storage.sync.set({rules: rules});
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {urlMatches: '.*'},
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
+
+
+chrome.storage.sync.get('rules', ({rules: loadedRules}) => {
+    console.log("loaded rules");
+    console.log(loadedRules);
+    syncedRule = loadedRules;
+});
---
+++ options.js
@@ -0,0 +1,146 @@
+class RulesBinder {
+    constructor(prefix = "short", {
+        redirectUrl = "https://github.com/",
+        matchRegex = "(.*)",
+        redirectRegexUrl = "https://github.com/$1"
+    }) {
+        let row = document.createElement("tr");
+        for (let i of [prefix, redirectUrl, matchRegex, redirectRegexUrl]) {
+            console.log(i);
+            let col = document.createElement("td");
+            let input = document.createElement("input");
+            input.value = i;
+            col.appendChild(input);
+            row.appendChild(col);
+        }
+
+        let col = document.createElement("td");
+        col.appendChild(this._delButton());
+        row.appendChild(col);
+
+        this._element = row;
+    }
+
+    _delButton() {
+        let button = document.createElement('button');
+        let icon = document.createElement('i');
+        icon.appendChild(document.createTextNode("delete"));
+        icon.className = "material-icons";
+        componentHandler.upgradeElement(icon);
+        button.appendChild(icon);
+        button.className = 'mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect';
+        componentHandler.upgradeElement(button);
+        button.addEventListener("click", () => {
+            this._element.remove();
+        });
+        return button;
+    }
+
+    row() {
+        return this._element;
+    }
+
+    rule() {
+        if (this._element == null)
+            return {"": {}};
+        return {
+            [this._element.children[0].firstChild.value]: {
+                redirectUrl: sanitiseURL(this._element.children[1].firstChild.value),
+                matchRegex: this._element.children[2].firstChild.value,
+                redirectRegexUrl: this._element.children[3].firstChild.value,
+            }
+        };
+    }
+}
+
+
+const content = document.getElementById("table");
+const newButton = document.getElementById("new");
+const saveButton = document.getElementById("save");
+const cancelButton = document.getElementById("cancel");
+
+chrome.storage.sync.get('rules', ({rules: rules}) => {
+    if (!Object.keys(rules).length) {
+        rules = {"": {}};
+    }
+
+    console.log(rules);
+
+    let controllers = Object.keys(rules).map(key => {
+        let controller = new RulesBinder(key, rules[key]);
+        content.appendChild(controller.row());
+        return controller;
+    });
+
+    newButton.addEventListener("click", () => {
+        let controller = new RulesBinder("", {});
+        content.appendChild(controller.row());
+        controllers.push(controller);
+    });
+
+    saveButton.addEventListener("click", () => {
+        let rules = {};
+        controllers.forEach(c => {
+            let value = c.rule();
+            let keys = Object.keys(value);
+            if (keys.indexOf("") === -1)
+                Object.assign(rules, value);
+        });
+        chrome.storage.sync.set({rules: rules}, () => chrome.runtime.reload());
+    });
+
+    cancelButton.addEventListener("click", () => {
+        chrome.runtime.reload();
+    });
+});
+
+function sanitiseURL(raw) {
+    if (!raw.match(/^\S+:\/\//)) {
+        return "https://" + raw;
+    }
+    return raw;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
